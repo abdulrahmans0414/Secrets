dea2de9cda1a77c906ca370526b60690250ff925
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-12 08:28:18 +0530

    add body-parser add fileURLToPath and dirname and get fetch data from index.html

--- index.js
+++ index.js
@@ -3,15 +3,23 @@
 //The password is ILoveCoding
 
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.get("/", (req, res) => {
-    res.json("success");
-  });
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
