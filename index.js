311ffa3672bd6530688633b704bea942a222f1c3
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-12 07:17:54 +0530

    add exprees and get methods to fetch the listen server.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 //To see how the final website should work, run "node index.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveCoding
+
+import express from "express";
+
+
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    res.json("success");
+  });
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
